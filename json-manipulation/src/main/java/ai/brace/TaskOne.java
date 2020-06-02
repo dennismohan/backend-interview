9adbcdcc3e62e9f07f5ed1fd9bc1551bb144776a
Dennis Mohan <dennis@ownits.com>
2020-06-02 15:43:31 -0400

    Add comment/todo for update Task 1 to leverage Task 2 functionality

--- json-manipulation/src/main/java/ai/brace/TaskOne.java
+++ json-manipulation/src/main/java/ai/brace/TaskOne.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 public class TaskOne {
 
+    // TODO: After implemented Task 2, it makes sense to update this code to use that same logic
+    // Note: given the local env issues, and number of tasks i've deprioritized updating this function
+    // should not take long to update, however I want to focus on features and little things add up! :).
     public static void execute(){
         String relativeFilePath = "src/main/resources/a1.json";
         List<BraceQuote> bookQuotes;
