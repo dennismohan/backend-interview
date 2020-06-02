56e6027ad5736a1be0c80172801a031b58f999db
Dennis Mohan <dennis@ownits.com>
2020-06-02 15:20:23 -0400

    Move some functionality to a Utils class as it will be used in both task one and task two

--- json-manipulation/src/main/java/ai/brace/TaskOne.java
+++ json-manipulation/src/main/java/ai/brace/TaskOne.java
@@ -18,35 +18,13 @@ public class TaskOne {
         String relativeFilePath = "src/main/resources/a1.json";
         List<BraceQuote> bookQuotes;
         try{
-            BraceBook book = TaskOne.getBraceBook(relativeFilePath);
+            BraceBook book = TaskUtils.getBraceBook(relativeFilePath);
             bookQuotes = Arrays.asList(book.getTextArray());
-        } catch(ReadBraceBookException err) {
+        } catch(TaskUtils.ReadBraceBookException err) {
             System.out.println("Failed to read " + relativeFilePath);
             return;
         }
 
-        Collections.sort(bookQuotes);
-        for(BraceQuote quote:bookQuotes){
-            System.out.println(quote.getTextData());
-        }
+        TaskUtils.sortAndPrint(bookQuotes);
     }
-
-    private static BraceBook getBraceBook(String relativePath) throws ReadBraceBookException {
-        BraceBook book;
-        Path a1JsonPath = Paths.get(relativePath);
-        Reader reader;
-        try {
-            reader = Files.newBufferedReader(a1JsonPath);
-            book = Main.gson.fromJson(reader, BraceBook.class);
-
-            reader.close();
-        }catch(IOException err) {
-            throw new ReadBraceBookException();
-        }
-        return book;
-    }
-
-    //  TODO: Should store some data for debugging: relative path, error message that occured
-    // (i believe stack is stored due to parent being Exception)
-    private static class ReadBraceBookException extends Exception{}
 }
